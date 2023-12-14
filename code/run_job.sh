c1247962bd2697391e72071675c36cb6d2f63fef
polina-tsvilodub <polina.tsvilodub@gmail.com>
2023-12-14 07:29:24 +0100

    sh script for iterating over all case studies

--- code/run_job.sh
+++ code/run_job.sh
@@ -26,5 +26,43 @@ echo " "
 # activate CUDA
 module load devel/cuda/11.6
 
-python3 -u main.py \
-    --model_name="meta-llama/Llama-2-7b-hf"
+# iterate over models
+models=("meta-llama/Llama-2-7b-hf" "meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-hf" "meta-llama/Llama-2-13b-chat-hf")
+# iterate over studies
+studies=("Deganoetal2024" "Martyetal2023" "Martyetal2022")
+# iterate over experiments
+expts2024=("Exp_2" "Exp_3")
+expts2023=("Exp_1" "Exp_2")
+expts2022=("Exp_4" "Exp_5" "Exp_6")
+
+for i in ${!models[*]}; do
+    echo "model: ${model[$i]}"
+    for j in ${!studies[$j]}; do
+        echo "study: ${studies[$j]}"
+        case "${studies[$j]}" in 
+            "Deganoetal2024")
+                for k in ${!expts2024[$k]}; do
+                python3 -u main.py \
+                    --model_name="${models[$i]}" \
+                    --study_name="${studies[]$j}" \
+                    --experiment_name="${expts2024[$k]}"
+                done
+            ;;
+            "Martyetal2023")
+                for k in ${!expts2023[$k]}; do
+                python3 -u main.py \
+                    --model_name="${models[$i]}" \
+                    --study_name="${studies[]$j}" \
+                    --experiment_name="${expts2023[$k]}"
+                done
+            ;;
+            "Martyetal2022")
+                for k in ${!expts2022[$k]}; do
+                python3 -u main.py \
+                    --model_name="${models[$i]}" \
+                    --study_name="${studies[]$j}" \
+                    --experiment_name="${expts2022[$k]}"
+                done
+        esac
+    done
+done
